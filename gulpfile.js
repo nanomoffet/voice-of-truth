29a1902bec4c0708576630979fdb4c3c0a5e0fd7
Michael Moffet <michael.moffet@broadbean.com>
2017-01-12 09:01:33 -0800

    initial commit of files

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var watch = require('gulp-watch');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
+var tsProject = ts.createProject('./src/tsconfig.json');
+var connect = require('gulp-connect');
+var deploy = require('gulp-gh-pages');
+/*
+ compile typescript
+ use ES5 and commonJS module
+ */
+gulp.task('typescript', function() {
+    var tsResult = tsProject.src().pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest('dist/js'));
+});
+/*
+ Web server to test app
+ */
+gulp.task('webserver', function() {
+    connect.server({
+        livereload: true,
+        root: ['.', 'dist']
+});
+});
+/*
+ Automatic Live Reload
+ */
+gulp.task('livereload', function() {
+    gulp.src(['dist/styles/*.css', 'dist/js/*.js'])
+     .pipe(watch(['dist/styles/*.css', 'dist/js/*.js']))
+     .pipe(connect.reload());
+     });
+     /*
+     copy all html files and assets
+     */
+    gulp.task('copy', function() {
+        gulp.src('src/**/*.html').pipe(gulp.dest('dist'));
+        gulp.src('src/assets/**/*.*').pipe(gulp.dest('dist/assets'));
+    });
+    /*
+     compile sass files
+     */
+    gulp.task('sass', function() {
+        gulp.src('src/styles/main.scss')
+    .pipe(sass())
+            .pipe(gulp.dest('dist/styles'));
+    });
+    /*
+     browserify
+     now is only for Javascript files
+     */
+    gulp.task('browserify', function() {
+        browserify('./dist/js/main.js').bundle().pipe(source('main.js')).pipe(gulp.dest('dist/js'));
+    });
+    /*
+     Watch typescript and sass
+     */
+    gulp.task('watch', function() {
+        gulp.watch('src/styles/scss/styles.scss', ['sass']);
+         gulp.watch('src/**/*.ts', ['typescript','browserify']);
+        gulp.watch('src/**/*.html', ['copy']);
+    })
+
+    gulp.task('deploy', function () {
+        return gulp.src("./dist/**/*")
+            .pipe(deploy())
+    });
+    /*
+     default task
+     */
+    gulp.task('default',['sass', 'typescript', 'browserify', 'copy', 'webserver', 'livereload', 'watch']);
+
+    /*Run everything except watch*/
+    gulp.task('compile',['sass', 'typescript', 'browserify', 'copy']);
